0dbb387331e027504ca0f92cfdacd03a596fab11
sihwa1019 <epretty@naver.com>
2022-07-15 16:40:57 +0900

    명령기능 구현 완료

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,4 +1,45 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class App {
+    private Scanner sc;
+    public App(){
+        sc = new Scanner(System.in);
+    }
+
+    public void run(){
+        System.out.println("== 명언 SSG==");
+        WiseSayingController wiseSayingController = new WiseSayingController(sc);
+
+        outer:
+        while(true){
+            System.out.print("명령) ");
+            String cmd = sc.nextLine();
+
+            Rq rq = new Rq(cmd);
+            switch (rq.getPath()){
+                case "등록" :
+                    wiseSayingController.add();
+                    break;
+
+                case "수정" :
+                    wiseSayingController.modify();
+                    break;
+
+                case "삭제" :
+                    wiseSayingController.delete();
+                    break;
+
+                case "목록" :
+                    wiseSayingController.list();
+                    break;
+
+                case "종료" :
+                    break outer;
+            }
+        }
+        sc.close();
+    }
+
 }
--- src/main/java/com/company/WiseSayingController.java
+++ src/main/java/com/company/WiseSayingController.java
@@ -1,4 +1,28 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class WiseSayingController {
+    private Scanner sc;
+    private WiseSayingRepository wiseSayingRepository;
+
+    public WiseSayingController(Scanner sc){
+        this.sc = sc;
+        wiseSayingRepository = new WiseSayingRepository();
+    }
+    public void add() {
+        System.out.println("등록완");
+    }
+
+    public void modify() {
+        System.out.println("수정완");
+    }
+
+    public void delete() {
+        System.out.println("삭제완");
+    }
+
+    public void list() {
+        System.out.println("목록출력완");
+    }
 }
